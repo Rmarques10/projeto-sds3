0fcaf221bfd0da341c5e0144ef49ccad1b852d13
Rafael Marques <rafaelmarques.rm86@gmail.com>
2021-11-05 11:29:03 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
-import { SalesSum } from 'types/sale';
+import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
@@ -16,7 +16,7 @@ const DonutChart = () => {
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then(response => {
-                const data = response.data as SalesSum[];
+                const data = response.data as SaleSum[];
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
 
@@ -24,13 +24,6 @@ const DonutChart = () => {
             });
 
     }, []);
-
-
-
-    // const mockData = {
-    //      series: [477138, 499928, 444867, 220426, 473088],
-    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
 
     const options = {
         legend: {
